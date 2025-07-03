4a60d9b9c2ca79e0663066cddbcc30ec00972a5a
Admin <lyngochieu0405@gmail.com>
2025-07-03 13:57:11 +0700

    connect to database

--- src/main/java/com/venho/laptopshop/demo/domain/User.java
+++ src/main/java/com/venho/laptopshop/demo/domain/User.java
@@ -1,7 +1,15 @@
 package com.venho.laptopshop.demo.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "User")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     private String email;
