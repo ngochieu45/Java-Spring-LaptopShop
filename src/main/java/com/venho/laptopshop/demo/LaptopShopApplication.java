aff94e96e3de8de38ae825c1af79c9a0cd9b89a1
Admin <lyngochieu0405@gmail.com>
2025-07-03 13:13:49 +0700

    add spring data jpa dependency

--- src/main/java/com/venho/laptopshop/demo/LaptopShopApplication.java
+++ src/main/java/com/venho/laptopshop/demo/LaptopShopApplication.java
@@ -3,8 +3,7 @@ package com.venho.laptopshop.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude =
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopShopApplication {
 
     public static void main(String[] args) {
