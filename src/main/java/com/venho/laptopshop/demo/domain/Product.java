23ce49e728f4104c3d7384aca796342ca6cc60af
Admin <lyngochieu0405@gmail.com>
2025-07-26 11:28:53 +0700

    remove unused import

--- src/main/java/com/venho/laptopshop/demo/domain/Product.java
+++ src/main/java/com/venho/laptopshop/demo/domain/Product.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "products")
