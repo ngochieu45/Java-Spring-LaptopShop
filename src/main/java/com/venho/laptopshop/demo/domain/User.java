2ef34d03c7ea896daeb1f38cad2fa30d70843a84
Admin <lyngochieu0405@gmail.com>
2025-08-15 18:00:04 +0700

    add cart and cartdetail model

--- src/main/java/com/venho/laptopshop/demo/domain/User.java
+++ src/main/java/com/venho/laptopshop/demo/domain/User.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
@@ -47,6 +48,9 @@ public class User {
     @OneToMany
     @JoinColumn(name = "user")
     List<Order> orders;
+
+    @OneToOne(mappedBy = "user")
+    private Cart cart;
 
     public Role getRole() {
         return role;
@@ -120,9 +124,18 @@ public class User {
         this.avatar = avatar;
     }
 
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", fullName=" + fullName + ", password=" + password
                 + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
+
 }
