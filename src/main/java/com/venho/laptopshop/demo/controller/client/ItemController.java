a5742d3deb4645b2d2f52491f1b01beb83157b52
Admin <lyngochieu0405@gmail.com>
2025-08-17 17:43:15 +0700

    add cart page

--- src/main/java/com/venho/laptopshop/demo/controller/client/ItemController.java
+++ src/main/java/com/venho/laptopshop/demo/controller/client/ItemController.java
@@ -42,4 +42,9 @@ public class ItemController {
         return "redirect:/";
     }
 
+    @GetMapping("/cart")
+    public String getCartPage(Model model) {
+        return "client/cart/show";
+    }
+
 }
